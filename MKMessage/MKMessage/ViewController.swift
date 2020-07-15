ddbb2e6239ce413988dc80e443a6da39e90af040
pritishsawant <pritish.sawant@upetch.com>
2020-07-15 17:24:57 +0530

    Project

---
+++ MKMessage/MKMessage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MKMessage
+//
+//  Created by Upetch Ventures on 15/07/20.
+//  Copyright © 2020 Upetch Ventures. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
